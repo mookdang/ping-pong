f18841dcf3582b248afe21c9a29b7f9e7560cf3b
Janet <janetkarpenske@gmail.com>
2020-06-22 15:13:34 -0700

    Will add main.js, styles.css, ping-pong.js code

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './styles.css';
+import { pingPong } from './ping-pong';
+
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    const goal = $('#goal').val();
+    const output = pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
